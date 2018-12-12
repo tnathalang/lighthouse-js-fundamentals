406e01a422f703260338662feebb7cc733c1491a
Tom Na <nathalang_t@hotmail.ca>
2018-12-12 21:00:58 +0000

    for loop excercise in reverse

---
+++ ingredients.js
@@ -0,0 +1,6 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("ingredients backward");
+for (i =ingredients.length-1 ; i >=0; i--) {
+  console.log(ingredients[i]);
+}
